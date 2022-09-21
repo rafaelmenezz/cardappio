066cd5e6786ba616fd4030e97775fe3846ca3357
fael_job <rafael.menezes@alunos.usj.edu.br>
2022-09-21 09:23:13 -0300

    Refactory

--- src/main/java/br/com/senac/dao/EnderecoDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.senac.dao;
-
-import br.com.senac.entidades.Endereco;
-
-public interface EnderecoDao extends BaseDao<Endereco, Long> {
-   
-}
---
+++ src/test/java/org/cardappio/CardappioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cardappio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardappioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
